d62a0b1a9e52afd9d3edac86bc71b7a401c90441
ssAkashi <74828087+ssAkashi@users.noreply.github.com>
2021-11-27 20:59:22 +0100

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,1 +1,1 @@
-ChristmasWebhook = "https://discord.com/api/webhooks/837354390527541268/gtFf3LbJfwFp4oeP8uuI0t7JkST3l3aM3ImInxeSpY6T0FmUR6FqAmekpsCFP0jkE7iJ" -- put your webhook link for calendar log
+ChristmasWebhook = "" -- put your webhook link for calendar log
